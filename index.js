809f0be5b0f6f9c221084374b719a1544d6dd6b3
蜡笔小秀 <sakura@labixiaoxiudeMacBook-Pro.local>
2017-05-03 18:46:02 +0800

    download

--- index.js
+++ index.js
@@ -69,17 +69,64 @@ const getMovieHTML = (source) => {
 
 // 循环遍历单个页面html，获取每部电影种子的下载链接
 const filterMovieLink = (tit, html) => {
-  sleep(3000);
+  sleep(3000)
   console.log(`正在抓取电影：《${tit}》`);
   const $ = cheerio.load(html);
-  const count = $('.tinfo').length;
+  const arr = $('.tinfo');
+  const count = arr.length > 5 ? 5 : arr.length;
   console.log(`《${tit}》共抓取到${count}个种子文件`);
+  const btArr = [];
+  for (let i = 0; i < count; i += 1) {
+    const btUrl = $(arr[i]).find('a').attr('href');
+    // btArr.push(btUrl);
+    const url = `${BASE_API}${btUrl}`;
+    getUrlDom(url).then((res) => {
+      // console.log(res);
+      filterDownLink(tit, res);
+    });
+  }
 }
 
-const sleep = (num) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, num);
+const filterDownLink = (tit, html) => {
+  sleep(3000);
+  console.log(`正在下载种子文件...`);
+  const $ = cheerio.load(html);
+  const downLink = $('form').attr('action');
+  console.log(downLink);
+  const url = `${BASE_API}/${downLink}`;
+  const data = {
+    action: 'download',
+    id: 24978,
+    uhash: '0cd8310fffa520dff7471f56',
+    'imageField.x': 61,
+    'imageField.y': 40,
+  }
+  const options = {
+    method: 'POST',
+    host: BASE_API,
+    path: downLink,
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Length': Buffer.byteLength(data),
+      'Content-Disposition': 'attachment; filename="%E3%80%90BT%E5%A4%A9%E5%A0%82%E3%80%91%E3%80%90BTtt.la%E3%80%91[720p]%E7%BB%BF%E5%B7%A8%E4%BA%BA%E5%A4%A7%E6%88%98.322.46MB.torrent"'
+    },
+  };
+  const req = http.request(options, (res) => {
+    res.on('data', (data) => {
+      console.log(data);
+    });
+    res.on('end', () => {
+      console.log('wanle');
+    });
   });
+  req.write(data);
+  req.end();
+}
+
+function sleep(ms) {
+  return function() {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, ms);
+    });
+  }
 }
